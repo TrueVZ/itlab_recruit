2ccf9689bb72409b719dd5ace6f37db6ecbeb01a
Zaikin Valentin <zaikinvala59@gmail.com>
2021-03-14 00:46:24 +0300

    Add: documentation for purchases

--- purchases_service/app/__init__.py
+++ purchases_service/app/__init__.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flasgger import Swagger
+from flasgger.utils import apispec_to_template
+from apispec import APISpec
+from apispec_webframeworks.flask import FlaskPlugin
+from apispec.ext.marshmallow import MarshmallowPlugin
 from .config import DevConfig, Test
 from app.extensions import db, migrate, ma
 from .models import *
+from .schemas import *
+from .validation import *
 
 import app.routes
+
+spec = APISpec(
+    title="PurchasesService API",
+    version='0.0.1',
+    openapi_version='2.0',
+    plugins=[
+        FlaskPlugin(),
+        MarshmallowPlugin(),
+    ],
+)
 
 
 def create_app(testing=False):
@@ -13,4 +30,11 @@ def create_app(testing=False):
     db.init_app(app)
     migrate.init_app(app, db)
     ma.init_app(app)
+    template = apispec_to_template(
+        app=app,
+        spec=spec,
+        definitions=[CreateCheckSchema, CheckSchema, UserSchema]
+    )
+    app.config['SWAGGER'] = {'uiversion': 3}
+    swag = Swagger(app, template=template)
     return app
--- purchases_service/app/routes.py
+++ purchases_service/app/routes.py
@@ -1,5 +1,5 @@
 from app.models import User, Check, Purchase
-from app.schemas import user_schema, users_schema, purchases_schema, check_schema
+from app.schemas import user_schema, users_schema, purchases_schema, check_schema, checks_schema
 from app.validation import CreateCheckSchema
 from app import db
 from flask import Blueprint, jsonify
@@ -13,6 +13,30 @@ bp = Blueprint("purchases", __name__)
 @bp.route("/user/create", methods=["POST"])
 @use_args({"username": fields.String(required=True)}, location="json")
 def add_user(args):
+    """
+    Create user
+    ---
+      parameters:
+        - name: username
+          in: body
+          type: object
+          properties:
+            username:
+              type: integer
+          required: true
+      responses:
+        '200':
+          description: OK
+          schema:
+            $ref: '#/definitions/User'
+        '400':
+          description: Bad request
+        '404':
+          description: User not found
+        '5xx':
+          description: Unexpected error
+
+    """
     try:
         user = User(username=args["username"])
         db.session.add(user)
@@ -25,6 +49,26 @@ def add_user(args):
 
 @bp.route("/user/<int:user_id>", methods=["GET"])
 def get_user_by_id(user_id):
+    """
+    Get user by id
+    ---
+      parameters:
+        - name: userID
+          in: path
+          type: integer
+          required: true
+      responses:
+        '200':
+          description: OK
+          schema:
+            $ref: '#/definitions/User'
+        '400':
+          description: Bad request
+        '404':
+          description: User not found
+        '5xx':
+          description: Unexpected error
+    """
     user = User.query.get(user_id)
     if user is not None:
         return user_schema.dump(user)
@@ -34,6 +78,17 @@ def get_user_by_id(user_id):
 
 @bp.route("/user/", methods=["GET"])
 def get_all_user():
+    """
+    Get all users
+    ---
+    responses:
+      '200':
+        description: OK
+        schema:
+          type: array
+          items:
+            $ref: '#/definitions/User'
+    """
     users = User.query.all()
     return jsonify(users=users_schema.dump(users)), 200
 
@@ -41,6 +96,33 @@ def get_all_user():
 @bp.route("/user/<int:user_id>/check/buy/", methods=["POST"])
 @use_args(CreateCheckSchema)
 def add_check(args, user_id):
+    """
+    Create check
+    ---
+    parameters:
+      - name: userID
+        in: path
+        required: true
+        type: integer
+      - name: check
+        in: body
+        schema:
+          $ref: '#/definitions/CreateCheck'
+    responses:
+      '200':
+        description: OK
+        schema:
+          $ref: '#/definitions/User'
+      '400':
+        description: Bad request
+      '404':
+        description: User not found
+      '5xx':
+        description: Unexpected error
+
+
+
+    """
     user = User.query.get(user_id)
     if user is None:
         db.session.rollback()
@@ -50,3 +132,9 @@ def add_check(args, user_id):
     db.session.add(check)
     db.session.commit()
     return check_schema.dump(check)
+
+
+# @bp.route("/user/<int:user_id/check/", methods=["GET"])
+# def get_user_checks(user_id):
+#     checks = Check.query.filter_by(user_id=user_id).all()
+#     return jsonify(checks=checks_schema.dump(checks))
