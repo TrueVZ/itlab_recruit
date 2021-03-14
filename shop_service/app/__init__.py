d2d55a31b0f328bf9c0367ff487fff67376b5e97
Zaikin Valentin <zaikinvala59@gmail.com>
2021-03-14 13:07:26 +0300

    Add: documentation for shop_service

--- shop_service/app/__init__.py
+++ shop_service/app/__init__.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from apispec import APISpec
+from apispec_webframeworks.flask import FlaskPlugin
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flasgger import Swagger
+from flasgger.utils import apispec_to_template
 from .config import DevConfig, Test
 from app.extensions import db, migrate, ma
+
 from .models import *
+from .schemas import *
+from .validation import *
 
 import app.routes
+
+
+spec = APISpec(
+    title="PurchasesService API",
+    version="0.0.1",
+    openapi_version="2.0",
+    plugins=[
+        FlaskPlugin(),
+        MarshmallowPlugin(),
+    ],
+)
 
 
 def create_app(testing=False):
@@ -13,4 +32,16 @@ def create_app(testing=False):
     db.init_app(app)
     migrate.init_app(app, db)
     ma.init_app(app)
+    template = apispec_to_template(
+        app=app,
+        spec=spec,
+        definitions=[
+            CreatePurchaseSchema,
+            CreateShopSchema,
+            ShopSchema,
+            PurchaseSchema,
+        ],
+    )
+    app.config["SWAGGER"] = {"uiversion": 3}
+    swag = Swagger(app, template=template)
     return app
--- shop_service/app/routes.py
+++ shop_service/app/routes.py
@@ -1,13 +1,24 @@
 from app.models import Purchase, Shop, Product
-from app.schemas import purchases_schema, shop_schema, product_schema, purchase_out_schema
-from app.validation import *
+from app.schemas import (
+    purchases_schema,
+    shop_schema,
+    product_schema,
+    products_schema,
+    purchase_out_schema,
+)
+from app.validation import (
+    CreatePurchaseSchema,
+    CreateShopSchema,
+    CreateProductSchema,
+    DeliveryProductSchema,
+    BuyInputSchema,
+)
 from app import db
 from flask import Blueprint, jsonify
 from webargs import fields
-from webargs.flaskparser import use_args
+from webargs.flaskparser import use_args, use_kwargs
 from sqlalchemy.exc import IntegrityError
 import requests
-
 
 bp = Blueprint("shop", __name__)
 
@@ -27,9 +38,30 @@ def add_shop(args):
 
 @bp.route("/shop/<int:shop_id>/", methods=["GET"])
 def get_shop(shop_id):
+    """
+    Get shop by id
+    ---
+      parameters:
+        - name: shopID
+          in: path
+          type: integer
+          required: true
+      responses:
+        '200':
+          description: OK
+          schema:
+            $ref: '#/definitions/Shop'
+        '400':
+          description: Bad request
+        '404':
+          description: Shop not found
+        '5xx':
+          description: Unexpected error
+
+    """
     shop = Shop.query.get(shop_id)
     if shop is not None:
-        return shop_schema.load(shop)
+        return shop_schema.dump(shop)
     return jsonify(message="Shop not found"), 404
 
 
@@ -37,19 +69,99 @@ def get_shop(shop_id):
 @use_args({"products": fields.Nested(CreateProductSchema, many=True)}, location="json")
 def add_product(args, shop_id):
     shop = Shop.query.get(shop_id)
-    for p in args["products"]:
-        product = product_schema.load(p)
-        product.shop = shop
-        db.session.add(product)
-        db.session.commit()
-
+    try:
+        for p in args["products"]:
+            product = product_schema.load(p)
+            product.shop = shop
+            db.session.add(product)
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify(message="Name product already exists")
+    db.session.commit()
     return shop_schema.dump(shop)
 
 
 @bp.route("/shop/<int:shop_id>/history/", methods=["GET"])
 def get_history(shop_id):
+    """
+    History of purchases
+    ---
+      parameters:
+        - name: shopID
+          in: path
+          type: integer
+          required: true
+      responses:
+        '200':
+          description: OK
+          schema:
+            type: array
+            items:
+              $ref: '#/definitions/Purchase'
+        '400':
+          description: Bad request
+        '404':
+          description: Shop not found
+        '5xx':
+          description: Unexpected error
+
+    """
     purchases = Purchase.query.filter_by(shop_id=shop_id).all()
     return jsonify(purchases=purchases_schema.dump(purchases))
+
+
+@bp.route("/shop/<int:shop_id>/product/", methods=["GET"])
+@use_kwargs({"category": fields.Str(required=False)}, location="query")
+def get_product_category(shop_id, category=None):
+    """
+    Get products from shop
+    ---
+      parameters:
+        - name: shopID
+          in: path
+          type: integer
+          required: true
+        - name: category
+          in: body
+          type: object
+          properties:
+            categoryName:
+              type: string
+          required: true
+      responses:
+        '200':
+          description: OK
+          schema:
+            type: array
+            items:
+              $ref: '#/definitions/Product'
+        '400':
+          description: Bad request
+        '404':
+          description: Shop not found
+        '5xx':
+          description: Unexpected error
+
+    """
+    if category is not None:
+        products = Product.query.filter_by(shop_id=shop_id, category=category).all()
+    else:
+        products = Product.query.filter_by(shop_id=shop_id).all()
+
+    return jsonify(products=products_schema.dump(products))
+
+
+@bp.route("/shop/<int:shop_id>/delivery/", methods=["PUT"])
+@use_args({"products": fields.Nested(DeliveryProductSchema(many=True))})
+def delivery(args, shop_id):
+    for p in args["products"]:
+        product = Product.query.filter_by(name=p["name"], shop_id=shop_id).first()
+        if product is None:
+            db.session.rollback()
+            return "Product not found", 404
+        product.count += p["count"]
+    db.session.commit()
+    return "Ok", 200
 
 
 @bp.route("/buy/", methods=["PUT"])
